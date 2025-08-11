ac56a52e92ee1e6a12a10fb5fbc18835dd75666e
Tan Kim <letankim2003@gmail.com>
2025-08-11 21:22:54 +0700

    update

--- pages/api/proxy/[[...path]].ts
+++ pages/api/proxy/[[...path]].ts
@@ -31,17 +31,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const fullPath = Array.isArray(pathParts) ? pathParts.join("/") : pathParts;
 
-  let latestIP = "127.0.0.1";
-  try {
-    const ipRes = await fetch("https://3docorp.id.vn/ip_handler.php");
-    const ipData = await ipRes.json();
-    if (ipData?.ip) {
-      latestIP = ipData.ip;
-    }
-  } catch (err) {
-    console.warn("⚠️ Không thể gọi ip_handler.php:", err);
-  }
-
   // const targetUrl = `https://facilitate-dui-investigate-aye.trycloudflare.com/api/v1/${fullPath}${
   //   queryString ? `?${queryString}` : ""
   // }`;
